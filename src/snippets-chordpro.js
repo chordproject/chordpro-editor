d80e20d30503384c8743165fa06917b8767ee930
Miguel Forget <tictactiptop@hotmail.com>
2021-03-03 17:33:55 +0100

    Update the snippets

--- src/snippets-chordpro.js
+++ src/snippets-chordpro.js
@@ -1,8 +1,56 @@
 import * as ace from 'ace-builds';
 
-ace.define('ace/snippets/chordpro', ['require', 'exports', 'module'], function(require, exports, module) {
+ace.define('ace/snippets/chordpro', ['require', 'exports', 'module'], function (require, exports, module) {
 
     exports.snippetText = [
+        // album tag
+        'snippet album',
+        '	{album: ${1:title}}',
+
+        // arranger tag
+        'snippet arranger',
+        '	{arranger: ${1:name}}',
+
+        // artist tag
+        'snippet a',
+        '	{artist: ${1:name}}',
+        'snippet artist',
+        '	{artist: ${1:name}}',
+
+        // capo tag
+        'snippet capo',
+        '	{capo: ${1:5}}',
+
+        // composer tag
+        'snippet composer',
+        '	{composer: ${1:name}}',
+
+        // copyright tag
+        'snippet copyright',
+        '	{copyright: ${1:copryight}}',
+
+        // duration tag
+        'snippet duration',
+        '	{duration: ${1:4}:${2:00}}',
+
+        // key tag
+        'snippet k',
+        '	{key: ${1:Am}}',
+        'snippet key',
+        '	{key: ${1:Am}}',
+
+        // lyricist tag
+        'snippet lyricist',
+        '	{lyricist: ${1:name}}',
+
+        // tempo tag
+        'snippet tempo',
+        '	{tempo: ${1:120}}',
+
+        // time tag
+        'snippet time',
+        '	{time: ${1:4}/${2:4}}',
+
         // title tag
         'snippet t',
         '	{title: ${1:title}}',
@@ -12,22 +60,12 @@ ace.define('ace/snippets/chordpro', ['require', 'exports', 'module'], function(r
         // subtitle tag
         'snippet st',
         '	{subtitle: ${1:name}}',
-        'snippet sub',
-        '	{subtitle: ${1:name}}',
         'snippet subtitle',
         '	{subtitle: ${1:name}}',
 
-        // artist tag
-        'snippet a',
-        '	{artist: ${1:name}}',
-        'snippet artist',
-        '	{artist: ${1:name}}',
-
-        // album tag
-        'snippet al',
-        '	{album: ${1:title}}',
-        'snippet album',
-        '	{album: ${1:title}}',
+        // year tag
+        'snippet year',
+        '	{year: ${1:2020}}',
 
         // comment tag
         'snippet c',
@@ -35,17 +73,38 @@ ace.define('ace/snippets/chordpro', ['require', 'exports', 'module'], function(r
         'snippet comment',
         '	{comment: ${1:description}}',
 
+
+
         // chorus block
         'snippet soc',
         '	{start_of_chorus}',
         'snippet eoc',
         '	{end_of_chorus}',
         'snippet chorus',
-        '	{start_of_chorus}',
-        '	{comment: ${1:Chorus}}',
-        '	${2:Music}',
+        '	{start_of_chorus: ${1:Chorus}}',
+        '	${2:lyrics}',
         '	{end_of_chorus}',
 
+        // verse block
+        'snippet sov',
+        '	{start_of_verse}',
+        'snippet eov',
+        '	{end_of_verse}',
+        'snippet verse',
+        '	{start_of_verse: ${1:Verse} ${2:1}}',
+        '	${3:lyrics}',
+        '	{end_of_verse}',
+
+        // bridge block
+        'snippet sob',
+        '	{start_of_bridge}',
+        'snippet eob',
+        '	{end_of_bridge}',
+        'snippet bridge',
+        '	{start_of_bridge: ${1:Bridge}}',
+        '	${2:lyrics}',
+        '	{end_of_bridge}',
+        
         // tabs block
         'snippet sot',
         '	{start_of_tab}',
@@ -53,17 +112,17 @@ ace.define('ace/snippets/chordpro', ['require', 'exports', 'module'], function(r
         '	{end_of_tab}',
         'snippet tab',
         '	{start_of_tab}',
-        '	A|-${1:-}--------------------------------|',
+        '	e|----------------------------------|',
+        '	B|----------------------------------|',
+        '	G|-${1:-}--------------------------------|',
+        '	D|----------------------------------|',
+        '	A|----------------------------------|',
         '	E|----------------------------------|',
-        '	C|----------------------------------|',
-        '	G|----------------------------------|',
         '	{end_of_tab}',
 
         // define tag
         'snippet d',
         '	{define: ${1:name} frets ${2:G_fretNum} ${3:C_fretNum} ${4:E_fretNum} ${5:A_fretNum} fingers ${6:fingerNum} ${7:fingerNum} ${8:fingerNum} ${9:fingerNum}}',
-        'snippet add',
-        '	add: string ${1:stringNum} fret ${2:fretNum} finger ${3:fingerNum}',
 
         // single-liners
         'snippet col',
