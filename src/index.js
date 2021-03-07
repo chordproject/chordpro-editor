3f5fa4fc0cbf76d61f48a99b5676c1440dba9bf5
Mora, Jaime <JaimeRosemberg.MoraJuez@ia.ca>
2021-03-06 21:37:53 -0500

    v0.1.3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-/**
- * <ul>
- * <li>Project: ChordProject Editor (based on UkeGeeks' Song-a-matic Editor from Buz Carter)</li>
- * <li>Author: Jaime Mora</li>
- * <li>License MIT</li>
- * </ul>
- *
- * <h3>Overview</h3>
- * Use Ace editor with a custom mode for Chordpro song texts.
- **/
-
 export { Main } from './main';
 export { Settings } from './settings';
